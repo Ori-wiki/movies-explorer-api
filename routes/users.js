4ff3fd5fb65e3975714b695d0d79dba262e43a20
Denis <Ori-wiki@gmail.com>
2023-06-10 19:49:00 +0300

    add route movies

---
+++ routes/users.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+
+const { getUserInfo, updateUserInfo } = require('../controllers/users');
+
+router.get('/users/me', getUserInfo);
+
+router.patch('/users/me', updateUserInfo);
+
+module.exports = router;
