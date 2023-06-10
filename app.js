74746ff6ad7ec748613eb4db6f4397bf5e983e8a
Denis <Ori-wiki@gmail.com>
2023-06-10 18:58:19 +0300

    add  .gitignore, .editorconfig, .eslintrc + app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const process = require('process');
+
+const app = express();
+
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+
+mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.listen(PORT);
