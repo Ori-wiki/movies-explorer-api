41794e702f73f612faa4aa9dac1d331b2bad879b
Denis <Ori-wiki@gmail.com>
2023-06-11 18:11:20 +0300

    updateUserValidation fix validation

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -23,7 +23,7 @@ const signUp = celebrate({
 const updateUserValidation = celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    about: Joi.string().required().min(2).max(30),
+    email: Joi.string().required().min(2).max(30),
   }),
 });
 
